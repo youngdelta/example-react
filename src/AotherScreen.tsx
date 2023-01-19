0ac8463869e840ff3e7c62c3905ac3bc901dfb5a
_ <youngdelta@gmail.com>
2023-01-19 15:29:41 +0900

    react-example

---
+++ src/AotherScreen.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const AotherScreen: FC = () => {
+	return <div>Hello World! Another Screen</div>;
+};
+
+export default AotherScreen;
---
+++ src/home.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-6 offset-md-3 col-sm-12 offset-sm-1 col-xs-12">Home - Hello World!</div>
+			</div>
+		</div>
+	);
+};
+/* 
+export default function home() {
+	return <div>home</div>;
+} */
+
+export default Home;
